bc0bab4cd96077340ae67ee7ba2b9d71a8cf162e
p1npo1nt <48339921+p1npo1nt@users.noreply.github.com>
2022-12-13 21:06:00 -0500

    deleted RequestContext

--- footy1/footypoll/views.py
+++ footy1/footypoll/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .forms import PlayerForm, StatsForm
 from django.template import RequestContext
+
 def playerindex(request):
     form = PlayerForm
     context = {'form': form}
-    return render(request, 'footeh/index.html', context, RequestContext(request))
+    return render(request, 'footeh/index.html', context)
 
 # Create your views here.
