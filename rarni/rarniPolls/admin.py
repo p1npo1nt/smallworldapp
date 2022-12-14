0547c35cf7e4a88cf889848295e5770438dedbee
Rohan Arni <50297222+rtenacity@users.noreply.github.com>
2022-12-14 11:29:48 -0500

    working on form on static site

--- rarni/rarniPolls/admin.py
+++ rarni/rarniPolls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Email
 
 admin.site.register(User) #registers the User Class under the admin panel
+admin.site.register(Email)
 
